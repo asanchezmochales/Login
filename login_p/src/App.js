edba43c1fba0a16826025dc4001cdc92cf7b234b
Justin Jerez <76711971+justinjerezz@users.noreply.github.com>
2022-10-10 09:36:15 +0200

    Añadir Cabecera

--- login_p/src/App.js
+++ login_p/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import Idiomas from './componentes/idiomas';
-
+import Cabecera from './componentes/cabecera';
 
 function App() {
   return (
-    <div>
+
+      <div>
+      <Cabecera></Cabecera>
       <Idiomas></Idiomas>
-    </div>
+      </div>
   );
 }
 
